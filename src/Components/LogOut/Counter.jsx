8b2eae7395280481c916095d8190194a1bf45315
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2023-12-25 23:39:49 +0330

    useReducer with Object J49

--- src/Components/LogOut/Counter.jsx
+++ src/Components/LogOut/Counter.jsx
@@ -1,11 +1,18 @@
 import React, { useReducer } from "react";
-const init = 0;
+const init = {
+  value1: 0,
+  value2: 5,
+};
 const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
+  switch (action.type) {
+    case "increment1":
+      return { ...state, value1: state.value1 + action.val };
+    case "decrement1":
+      return { ...state, value1: state.value1 - action.val };
+    case "increment5":
+      return { ...state, value2: state.value2 + action.val };
+    case "decrement5":
+      return { ...state, value2: state.value2 - action.val };
     case "reset":
       return init;
     default:
@@ -16,27 +23,44 @@ const Counter = () => {
   const [count, dispatch] = useReducer(reducer, init);
   return (
     <div>
-      <h2 className="h2-counter">{count}</h2>
+      <h2 className="h2-counter">{count.value1}</h2>
       <button
         className="btnCountBody btn-margin"
         onClick={() => {
-          dispatch("increment");
+          dispatch({ type: "increment1", val: 1 });
         }}
       >
-        افزایش
+        +1 افزایش
       </button>
       <button
         className="btnCountBody btn-margin"
         onClick={() => {
-          dispatch("decrement");
+          dispatch({ type: "decrement1", val: 1 });
         }}
       >
-        کاهش
+        -1 کاهش
+      </button>
+      <h2 className="h2-counter">{count.value2}</h2>
+      <button
+        className="btnCountBody btn-margin"
+        onClick={() => {
+          dispatch({ type: "increment5", val: 5 });
+        }}
+      >
+        +5 افزایش
       </button>
       <button
         className="btnCountBody btn-margin"
         onClick={() => {
-          dispatch("reset");
+          dispatch({ type: "decrement5", val: 5 });
+        }}
+      >
+        -5 کاهش
+      </button>
+      <button
+        className="btnCountBody btn-margin"
+        onClick={() => {
+          dispatch({ type: "reset" });
         }}
       >
         ریست
